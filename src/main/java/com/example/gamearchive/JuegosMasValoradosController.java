982ec24df4c179f04e245bb7039cfd4ca42e6d05
xianDT <xiandt01@gmail.com>
2024-05-22 20:36:18 +0200

    Menu juegos mas valorados remodelado

--- src/main/java/com/example/gamearchive/JuegosMasValoradosController.java
+++ src/main/java/com/example/gamearchive/JuegosMasValoradosController.java
@@ -10,8 +10,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -27,102 +31,109 @@ public class JuegosMasValoradosController implements Initializable {
 
     @FXML
     private Button Volver;
+
     @FXML
-    private ImageView Top1;
-    @FXML
-    private ImageView Top2;
-    @FXML
-    private ImageView Top3;
-    @FXML
-    private ImageView Top4;
-    @FXML
-    private ImageView Top5;
-    @FXML
-    private ImageView Top6;
-    @FXML
-    private ImageView Top7;
-    @FXML
-    private ImageView Top8;
-    @FXML
-    private ImageView Top9;
-    @FXML
-    private ImageView Top10;
+    private VBox contenedorJuegos; // Contenedor para agregar los elementos de los juegos
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try (Connection connection = DatabaseConnection.getConnection()) {
-            String query = "SELECT idJuego, rutaCaratula FROM Juegos " +
-                    "ORDER BY (SELECT AVG(calificacion) FROM Reseñas WHERE Reseñas.idJuego = Juegos.idJuego) DESC " +
-                    "LIMIT 10";
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-
-            Map<ImageView, Integer> imageViewMap = new HashMap<>();
-            imageViewMap.put(Top1, null);
-            imageViewMap.put(Top2, null);
-            imageViewMap.put(Top3, null);
-            imageViewMap.put(Top4, null);
-            imageViewMap.put(Top5, null);
-            imageViewMap.put(Top6, null);
-            imageViewMap.put(Top7, null);
-            imageViewMap.put(Top8, null);
-            imageViewMap.put(Top9, null);
-            imageViewMap.put(Top10, null);
-
-            int index = 0;
-            while (resultSet.next() && index < 10) {
-                int idJuego = resultSet.getInt("idJuego");
-                String rutaCaratula = resultSet.getString("rutaCaratula");
-                ImageView imageView = getTopImageViewByIndex(index);
-                imageViewMap.put(imageView, idJuego);
-                // Cargar la imagen del juego desde la rutaCaratula y asignarla al ImageView
-                File file = new File(rutaCaratula);
-                Image image = new Image(file.toURI().toString());
-                imageView.setImage(image);
-                // Asignar evento de clic para abrir la pantalla del juego correspondiente
-                asignarEventoClic(imageView, idJuego);
-
-                index++;
-            }
+        try {
+            mostrarJuegosMasValorados();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    private void mostrarJuegosMasValorados() throws SQLException {
+        String query = "SELECT J.idJuego, J.nombre, J.descripcion, J.rutaCaratula, J.plataformas, " +
+                "(SELECT AVG(R.calificacion) FROM Reseñas R WHERE R.idJuego = J.idJuego) AS notaMedia " +
+                "FROM Juegos J " +
+                "ORDER BY notaMedia DESC " +
+                "LIMIT 10";
 
-    private ImageView getTopImageViewByIndex(int index) {
-        switch (index) {
-            case 0:
-                return Top1;
-            case 1:
-                return Top2;
-            case 2:
-                return Top3;
-            case 3:
-                return Top4;
-            case 4:
-                return Top5;
-            case 5:
-                return Top6;
-            case 6:
-                return Top7;
-            case 7:
-                return Top8;
-            case 8:
-                return Top9;
-            case 9:
-                return Top10;
-            default:
-                return null;
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int idJuego = resultSet.getInt("idJuego");
+                String nombre = resultSet.getString("nombre");
+                String descripcion = resultSet.getString("descripcion");
+                String rutaCaratula = resultSet.getString("rutaCaratula");
+                String plataformas = resultSet.getString("plataformas");
+                double notaMedia = resultSet.getDouble("notaMedia");
+
+                // Crear elementos para mostrar el juego
+                ImageView imageView = new ImageView(new Image(new File(rutaCaratula).toURI().toString()));
+                imageView.setFitHeight(150);
+                imageView.setFitWidth(100);
+                asignarEventoClic(imageView, idJuego);
+
+                Label labelNombre = new Label(nombre);
+                labelNombre.setStyle("-fx-font-size: 18px; -fx-font-weight: bold; -fx-text-fill: white;");
+
+                Label labelNotaMedia = new Label(String.format("%.2f", notaMedia));
+                labelNotaMedia.setStyle("-fx-font-size: 18px; -fx-background-radius: 5; -fx-padding: 5;");
+                if (notaMedia >= 0 && notaMedia < 5) {
+                    labelNotaMedia.setStyle(labelNotaMedia.getStyle() + "-fx-background-color: #FF6874; -fx-text-fill: black;");
+                } else if (notaMedia >= 5 && notaMedia < 8) {
+                    labelNotaMedia.setStyle(labelNotaMedia.getStyle() + "-fx-background-color: #FFBD3F; -fx-text-fill: black;");
+                } else if (notaMedia >= 8 && notaMedia < 9) {
+                    labelNotaMedia.setStyle(labelNotaMedia.getStyle() + "-fx-background-color: #00CE7A; -fx-text-fill: black;");
+                } else if (notaMedia >= 9) {
+                    labelNotaMedia.setStyle(labelNotaMedia.getStyle() + "-fx-background-color: #00CE7A; -fx-text-fill: gold;");
+                }
+
+                HBox nombreNotaBox = new HBox(10, labelNombre, labelNotaMedia);
+
+                Label labelPlataformas = new Label("Plataformas: " + plataformas);
+                labelPlataformas.setStyle("-fx-font-size: 16px; -fx-text-fill: white;");
+                Label labelDescripcion = new Label(descripcion);
+                labelDescripcion.setWrapText(true);
+                labelDescripcion.setMaxWidth(Double.MAX_VALUE); // Permite que la descripción ocupe todo el ancho disponible
+                labelDescripcion.setStyle("-fx-font-size: 16px; -fx-text-fill: white;");
+
+                VBox.setVgrow(labelDescripcion, Priority.ALWAYS); // Permite que la descripción crezca en el VBox si es necesario
+
+                VBox detallesBox = new VBox(10, nombreNotaBox, labelPlataformas, labelDescripcion);
+                HBox.setHgrow(detallesBox, Priority.ALWAYS); // Permite que el VBox crezca en el HBox si es necesario
+
+                HBox juegoBox = new HBox(10, imageView, detallesBox);
+                juegoBox.setStyle("-fx-padding: 10; -fx-background-color: #333; -fx-border-color: #555; -fx-border-width: 2px;");
+                juegoBox.setMinWidth(600); // Ajusta el ancho mínimo del contenedor según tus necesidades
+                contenedorJuegos.getChildren().add(juegoBox);
+            }
         }
     }
+
+
+
+
     private void asignarEventoClic(ImageView imageView, int idJuego) {
         imageView.setOnMouseClicked(event -> {
-            // Abrir la pantalla del juego cuando se hace clic en la imagen
             abrirMenuJuego(idJuego);
         });
+
+        // Añadir animación al pasar el ratón
+        imageView.setOnMouseEntered(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(200), imageView);
+            st.setToX(1.1);
+            st.setToY(1.1);
+            st.play();
+        });
+
+        imageView.setOnMouseExited(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.millis(200), imageView);
+            st.setToX(1.0);
+            st.setToY(1.0);
+            st.play();
+        });
     }
+
+
+
     private void abrirMenuJuego(int idJuego) {
-        try (Connection connection =DatabaseConnection.getConnection()) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
             String query = "SELECT idJuego, nombre, descripcion, fechaLanzamiento, rutaCaratula, plataformas " +
                     "FROM Juegos " +
                     "WHERE idJuego = ?";
@@ -147,9 +158,6 @@ public class JuegosMasValoradosController implements Initializable {
                 stage.setTitle("GameArchive");
                 stage.getIcons().add(new Image(getClass().getResourceAsStream("/img/logo-GameArchive.png")));
                 stage.setScene(new Scene(root));
-                Stage ventanaActual = (Stage) Top1.getScene().getWindow();
-                ventanaActual.close();
-
                 stage.show();
             } else {
                 System.out.println("No se encontró información para el juego con ID: " + idJuego);
@@ -158,7 +166,6 @@ public class JuegosMasValoradosController implements Initializable {
             e.printStackTrace();
         }
     }
-
 
     private double obtenerNotaMediaJuego(int idJuego) throws SQLException {
         double notaMedia = 0;
@@ -183,99 +190,6 @@ public class JuegosMasValoradosController implements Initializable {
 
         return notaMedia;
     }
-    @FXML
-    private void onMouseTop1() {
-        aplicarAnimacion(Top1);
-    }
-    @FXML
-    private void onMouseTop2() {
-        aplicarAnimacion(Top2);
-    }
-    @FXML
-    private void onMouseTop3() {
-        aplicarAnimacion(Top3);
-    }
-    @FXML
-    private void onMouseTop4() {
-        aplicarAnimacion(Top4);
-    }
-    @FXML
-    private void onMouseTop5() {
-        aplicarAnimacion(Top5);
-    }
-    @FXML
-    private void onMouseTop6() {
-        aplicarAnimacion(Top6);
-    }
-    @FXML
-    private void onMouseTop7() {
-        aplicarAnimacion(Top7);
-    }
-    @FXML
-    private void onMouseTop8() {
-        aplicarAnimacion(Top8);
-    }
-    @FXML
-    private void onMouseTop9() {
-        aplicarAnimacion(Top9);
-    }
-    @FXML
-    private void onMouseTop10() {
-        aplicarAnimacion(Top10);
-    }
-    @FXML
-    private void onMouseExitedTop1() {
-        quitarAnimacion(Top1);
-    }
-    @FXML
-    private void onMouseExitedTop2() {
-        quitarAnimacion(Top2);
-    }
-    @FXML
-    private void onMouseExitedTop3() {
-        quitarAnimacion(Top3);
-    }
-    @FXML
-    private void onMouseExitedTop4() {
-        quitarAnimacion(Top4);
-    }
-    @FXML
-    private void onMouseExitedTop5() {
-        quitarAnimacion(Top5);
-    }
-    @FXML
-    private void onMouseExitedTop6() {
-        quitarAnimacion(Top6);
-    }
-    @FXML
-    private void onMouseExitedTop7() {
-        quitarAnimacion(Top7);
-    }
-    @FXML
-    private void onMouseExitedTop8() {
-        quitarAnimacion(Top8);
-    }
-    @FXML
-    private void onMouseExitedTop9() {
-        quitarAnimacion(Top9);
-    }
-    @FXML
-    private void onMouseExitedTop10() {
-        quitarAnimacion(Top10);
-    }
-    private void aplicarAnimacion(ImageView imageView) {
-        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(200), imageView);
-        scaleTransition.setToX(1.2);
-        scaleTransition.setToY(1.2);
-        scaleTransition.play();
-    }
-
-    private void quitarAnimacion(ImageView imageView) {
-        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(200), imageView);
-        scaleTransition.setToX(1.0);
-        scaleTransition.setToY(1.0);
-        scaleTransition.play();
-    }
 
     @FXML
     private void handleVolverPantallaPrincipal(ActionEvent event) throws IOException {
@@ -287,5 +201,4 @@ public class JuegosMasValoradosController implements Initializable {
         ventana.setScene(scene);
         ventana.show();
     }
-
 }
